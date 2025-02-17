b483f1c6e2adb04af663bcfe1a1d7a6cdcf6d23b
ijimnun907 <ijimnun907@g.educaand.es>
2025-02-17 09:55:23 +0100

    Cambiar por formulario

--- src/Component/Body.jsx
+++ src/Component/Body.jsx
@@ -3,14 +3,31 @@ import Cabecera from "./Cabecera.jsx";
 import Banner from "./Banner.jsx";
 import Pie from "./Pie.jsx";
 import ContenidoPrincipal from "./ContenidoPrincipal.jsx";
+import {useState} from "react";
+import FormularioContacto from "./FormularioContacto.jsx";
 
 const Body = () => {
+    const [mostrarPagina, setmostrarPagina] = useState(0);
+
+    const manejarPagina = (numero) => {
+        setmostrarPagina(numero);
+    }
     return (
         <>
             <Cabecera />
+            <button type="button" onClick={() => manejarPagina(1)}>Ir a formulario</button>
+            <button type="button" onClick={() => manejarPagina(0)}>Ir a principal</button>
             <main>
                 <Banner titulo="La Primera cerveza Artesana y Natural de la Sierra de Gredos"/>
-                <ContenidoPrincipal />
+                {mostrarPagina === 0 ?
+                    <ContenidoPrincipal />
+                    :
+                    mostrarPagina === 1 ?
+                        <FormularioContacto />
+                        :
+                        <ContenidoPrincipal />
+                }
+
             </main>
             <Pie />
         </>
